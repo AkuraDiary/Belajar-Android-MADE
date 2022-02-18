f7df85b7b6dd476094ba3230ffc2e3b32e51d650
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-19 03:27:49 +0700

    added View Models

--- MySimpleCleanArchitecture/app/src/main/java/com/example/mysimplecleanarchitecture/data/MessageRepository.kt
+++ MySimpleCleanArchitecture/app/src/main/java/com/example/mysimplecleanarchitecture/data/MessageRepository.kt
@@ -3,7 +3,7 @@ package com.example.mysimplecleanarchitecture.data
 import com.example.mysimplecleanarchitecture.domain.IMessageRepository
 import com.example.mysimplecleanarchitecture.domain.MessageEntity
 
-class MessageRepository(private val messageDataSource: MessageDataSource) : IMessageRepository{
+class MessageRepository(private val messageDataSource: IMessageDataSource) : IMessageRepository{
     override fun getWelcomeMessage(name: String): MessageEntity {
         return messageDataSource.getMessageFromSource(name)
     }

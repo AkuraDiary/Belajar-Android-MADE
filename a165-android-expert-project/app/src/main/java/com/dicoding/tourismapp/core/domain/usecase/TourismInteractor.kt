b82061154db5ed8dfa44d89001b2ffb41841bee5
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-21 02:53:05 +0700

    changed livedata into flowable

--- a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
+++ a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.LiveData
 import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.model.Tourism
+import io.reactivex.Flowable
 
 class TourismInteractor(private val tourismRepository: TourismRepository) :  TourismUseCase{
-    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> {
+    override fun getAllTourism(): Flowable<Resource<List<Tourism>>> {
         return tourismRepository.getAllTourism()
     }
 
-    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
+    override fun getFavoriteTourism(): Flowable<List<Tourism>> {
         return tourismRepository.getFavoriteTourism()
     }
 

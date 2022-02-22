498d1a5a39e2cbd5cc72160b76bc16e95cb24b9d
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-22 08:20:48 +0700

    implementing view model and added internet permission

--- MyReactiveSearch/app/src/main/java/com/example/myreactivesearch/MainActivity.kt
+++ MyReactiveSearch/app/src/main/java/com/example/myreactivesearch/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.myreactivesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import com.example.myreactivesearch.databinding.ActivityMainBinding
+import com.example.myreactivesearch.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    val viewModel : MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val edPlace = binding.edPlace
+        
+        edPlace.addTextChangedListener(object  : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                lifecycleScope.launch {
+                    viewModel.queryChannel.send(s.toString())
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        viewModel.searchResult.observe(this) { placesItem ->
+            val placesName = arrayListOf<String?>()
+
+            placesItem.map {
+                placesName.add(it.placeName)
+            }
+
+            val adapter =
+                ArrayAdapter(this@MainActivity, android.R.layout.select_dialog_item, placesName)
+            adapter.notifyDataSetChanged()
+            edPlace.setAdapter(adapter)
+        }
     }
 }

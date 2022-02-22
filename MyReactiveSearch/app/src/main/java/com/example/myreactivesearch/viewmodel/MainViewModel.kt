af180dcf2641e856a948f7da9be8a96819137bfd
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-22 08:20:26 +0700

    added viewModel

---
+++ MyReactiveSearch/app/src/main/java/com/example/myreactivesearch/viewmodel/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.myreactivesearch.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.myreactivesearch.network.ApiConfig
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+class MainViewModel : ViewModel() {
+
+    private val accessToken = "pk.eyJ1IjoiYXN0aGlzZXRhIiwiYSI6ImNreng1YjBlMTAxdHkyeHBsdmcyYXN5MHAifQ.t7JoRn39sPoZzOr0uphCaQ"
+    val queryChannel = BroadcastChannel<String>(Channel.CONFLATED)
+
+    val searchResult = queryChannel.asFlow()
+        .debounce(300)
+        .distinctUntilChanged()
+        .filter {
+            it.trim().isNotEmpty()
+        }
+        .mapLatest {
+            ApiConfig.provideApiService().getCountry(it, accessToken).features
+        }
+        .asLiveData()
+
+}

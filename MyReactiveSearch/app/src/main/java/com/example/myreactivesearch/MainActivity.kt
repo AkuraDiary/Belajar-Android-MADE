886ce3ed22d2979127d5ac8562d82c2dc474e255
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-22 09:01:57 +0700

    it's done, but the autocomplete doesn't work as expected

--- MyReactiveSearch/app/src/main/java/com/example/myreactivesearch/MainActivity.kt
+++ MyReactiveSearch/app/src/main/java/com/example/myreactivesearch/MainActivity.kt
@@ -10,19 +10,23 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.example.myreactivesearch.databinding.ActivityMainBinding
 import com.example.myreactivesearch.viewmodel.MainViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     val viewModel : MainViewModel by viewModels()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val edPlace = binding.edPlace
+        val _edPlace = binding.edPlace
         
-        edPlace.addTextChangedListener(object  : TextWatcher {
+        _edPlace.addTextChangedListener(object  : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
             }
@@ -45,10 +49,9 @@ class MainActivity : AppCompatActivity() {
                 placesName.add(it.placeName)
             }
 
-            val adapter =
-                ArrayAdapter(this@MainActivity, android.R.layout.select_dialog_item, placesName)
+            val adapter = ArrayAdapter(this@MainActivity, android.R.layout.select_dialog_item, placesName)
             adapter.notifyDataSetChanged()
-            edPlace.setAdapter(adapter)
+            _edPlace.setAdapter(adapter)
         }
     }
 }

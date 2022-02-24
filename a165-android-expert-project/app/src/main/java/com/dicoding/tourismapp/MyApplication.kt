3c5ddb5f772bdc64ec85d0cbdba381040a14d441
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-24 21:44:17 +0700

    welp, i dont know what am i doin right now

--- a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
+++ a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
@@ -1,31 +1,22 @@
 package com.dicoding.tourismapp
 
 import android.app.Application
-import com.dicoding.tourismapp.core.di.databaseModule
-import com.dicoding.tourismapp.core.di.networkModule
-import com.dicoding.tourismapp.core.di.repositoryModule
-import com.dicoding.tourismapp.di.useCaseModule
-import com.dicoding.tourismapp.di.viewModelModule
+import com.dicoding.tourismapp.core.di.*
+import com.dicoding.tourismapp.di.AppComponent
+import com.dicoding.tourismapp.di.DaggerAppComponent
+
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-class MyApplication  : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        startKoin{
-            androidLogger(Level.NONE)
-            androidContext(this@MyApplication)
-            modules(
-                listOf(
-                    databaseModule,
-                    networkModule,
-                    repositoryModule,
-                    useCaseModule,
-                    viewModelModule
-                )
-            )
-        }
+open class MyApplication : Application() {
+
+    private val coreComponent: CoreComponent by lazy {
+        DaggerCoreComponent.factory().create(applicationContext)
+    }
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create(coreComponent)
     }
 }
--- a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/di/AppModule.kt
+++ a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/di/AppModule.kt
@@ -5,15 +5,24 @@ import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 import com.dicoding.tourismapp.detail.DetailTourismViewModel
 import com.dicoding.tourismapp.favorite.FavoriteViewModel
 import com.dicoding.tourismapp.home.HomeViewModel
+import dagger.Binds
+import dagger.Module
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val useCaseModule = module {
-    factory<TourismUseCase> {TourismInteractor(get())}
-}
+@Module
+abstract class AppModule{
 
-val viewModelModule = module {
-    viewModel { HomeViewModel(get()) }
-    viewModel { FavoriteViewModel(get()) }
-    viewModel { DetailTourismViewModel(get()) }
+    @Binds
+    abstract fun provideTourismUseCase(tourismInteractor: TourismInteractor) : TourismUseCase
+
+    val useCaseModule = module {
+        factory<TourismUseCase> {TourismInteractor(get())}
+    }
+
+    val viewModelModule = module {
+        viewModel { HomeViewModel(get()) }
+        viewModel { FavoriteViewModel(get()) }
+        viewModel { DetailTourismViewModel(get()) }
+    }
 }

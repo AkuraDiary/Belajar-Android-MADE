4cba5cdbce285cdfa9a0cf796f1553bcd68d66c8
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-24 21:16:58 +0700

    added core component

--- a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/core/data/source/local/LocalDataSource.kt
+++ a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/core/data/source/local/LocalDataSource.kt
@@ -3,8 +3,11 @@ package com.dicoding.tourismapp.core.data.source.local
 import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.data.source.local.room.TourismDao
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class LocalDataSource(private val tourismDao: TourismDao) {
+@Singleton
+class LocalDataSource @Inject constructor(private val tourismDao: TourismDao) {
 
     fun getAllTourism(): Flow<List<TourismEntity>> = tourismDao.getAllTourism()
 
--- a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
+++ a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -8,8 +8,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class RemoteDataSource(private val apiService: ApiService) {
+@Singleton
+class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
 
 
     suspend fun getAllTourism(): Flow<ApiResponse<List<TourismResponse>>> {

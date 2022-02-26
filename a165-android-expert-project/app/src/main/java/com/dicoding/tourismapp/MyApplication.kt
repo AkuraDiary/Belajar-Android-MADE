926d1de53f74f44d770eac3cfb06e81a338e9302
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-26 09:06:27 +0700

    added hiltandroid app annotation

--- a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
+++ a165-android-expert-project/app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
@@ -4,19 +4,21 @@ import android.app.Application
 import com.dicoding.tourismapp.core.di.*
 import com.dicoding.tourismapp.di.AppComponent
 import com.dicoding.tourismapp.di.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
+@HiltAndroidApp
 open class MyApplication : Application() {
 
-    private val coreComponent: CoreComponent by lazy {
+    /*private val coreComponent: CoreComponent by lazy {
         DaggerCoreComponent.factory().create(applicationContext)
     }
 
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.factory().create(coreComponent)
-    }
+    }*/
 }

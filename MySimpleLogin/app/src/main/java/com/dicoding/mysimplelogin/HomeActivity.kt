4b5a2ff6c12cf2e080788779f0c40fa73c9e2ca8
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-27 22:46:21 +0700

    yey, chat feature

--- MySimpleLogin/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ MySimpleLogin/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -2,10 +2,12 @@ package com.dicoding.mysimplelogin
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.MyApplication
 import com.dicoding.core.UserRepository
 import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
+import java.lang.Exception
 import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity() {
@@ -27,10 +29,22 @@ class HomeActivity : AppCompatActivity() {
             userRepository.logoutUser()
             moveToMainActivity()
         }
+
+        binding.fab.setOnClickListener{
+            try {
+                moveToChatActivity()
+            }catch (e : Exception){
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun moveToMainActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
+    }
+
+    private fun moveToChatActivity(){
+        startActivity(Intent(this, Class.forName("com.dicoding.mysimplelogin.chat.ChatActivity")))
     }
 }

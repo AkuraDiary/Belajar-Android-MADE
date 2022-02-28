9651a4002c58fa7804fd0e8d2031dce5cb796f5a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-28 21:53:27 +0700

    fixed some coloring in splashscreen

--- Submission1MadeDicoding/app/src/main/java/com/asthiseta/submission1madedicoding/ui/SplashScreenActivity.kt
+++ Submission1MadeDicoding/app/src/main/java/com/asthiseta/submission1madedicoding/ui/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.view.animation.AnimationUtils
 import com.asthiseta.submission1madedicoding.R
 import com.asthiseta.submission1madedicoding.databinding.ActivitySplashScreenBinding
@@ -15,9 +16,11 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         splashBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+
         setContentView(splashBinding.root)
 
         supportActionBar?.hide() //hiding the action bar
+
         val logo = splashBinding.logo
         val slideAnim = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
         logo.startAnimation(slideAnim)

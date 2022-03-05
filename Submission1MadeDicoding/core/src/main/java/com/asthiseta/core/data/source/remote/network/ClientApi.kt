b6f8dd78017004dff20f13788a91e1b7c8fc653f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-05 19:42:15 +0700

    added search for item detail

--- Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/data/source/remote/network/ClientApi.kt
+++ Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/data/source/remote/network/ClientApi.kt
@@ -1,12 +1,24 @@
 package com.asthiseta.core.data.source.remote.network
 
+import com.asthiseta.core.data.source.remote.response.ItemResponse
+import com.asthiseta.core.data.source.remote.response.ListItemResponse
 import com.asthiseta.core.domain.model.Item
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ClientApi {
+
+    @GET("kosts")
+    suspend fun getAllKost() : ListItemResponse
+
     @GET("kosts")
     suspend fun searchForItem(
         @Query("q")q: String?
-    ): List<Item>
+    ): ListItemResponse
+
+   @GET("kosts")
+    suspend fun searchForItemDetail(
+        @Query("q")q: String?
+    ): ItemResponse
+
 }

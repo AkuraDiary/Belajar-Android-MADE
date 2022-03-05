3dabd866df7734c77ec93d21dfbe71ff048f4101
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-05 19:43:09 +0700

    implementing the methods

--- Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/data/ItemRepos.kt
+++ Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/data/ItemRepos.kt
@@ -1,32 +1,57 @@
 package com.asthiseta.core.data
 
+import com.asthiseta.core.data.source.NetworkResources
 import com.asthiseta.core.data.source.local.LocalDataSource
 import com.asthiseta.core.data.source.remote.RemoteDataSource
+import com.asthiseta.core.data.source.remote.network.ApiResponse
+import com.asthiseta.core.data.source.remote.response.ItemResponse
 import com.asthiseta.core.domain.model.Item
 import com.asthiseta.core.domain.repository.IItemRepository
+import com.asthiseta.core.misc.DataMapper
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class ItemRepos (
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource
     ) : IItemRepository {
     override fun getAllKos(query: String?): Flow<Resource<List<Item>>> {
-        TODO("Not yet implemented")
+        return object : NetworkResources<List<Item>, List<ItemResponse>>(){
+            override fun loadFromNetwork(data: List<ItemResponse>): Flow<List<Item>> {
+                return DataMapper.mapResponsesToDomain(data)
+            }
+
+            override suspend fun createCall(): Flow<ApiResponse<List<ItemResponse>>> {
+                return remoteDataSource.getAllItem(query)
+            }
+        }.asFlow()
     }
 
     override fun getDetailKos(name: String): Flow<Resource<Item>> {
-        TODO("Not yet implemented")
+        return object : NetworkResources<Item, ItemResponse>() {
+            override fun loadFromNetwork(data: ItemResponse): Flow<Item> {
+                return DataMapper.mapResponseToDomain(data)
+            }
+
+            override suspend fun createCall(): Flow<ApiResponse<ItemResponse>> {
+                return remoteDataSource.getItemDetail(name)
+            }
+        }.asFlow()
     }
 
     override fun getFavoriteKos(): Flow<List<Item>> {
-        TODO("Not yet implemented")
+        return localDataSource.getFavoriteItem().map {
+            DataMapper.mapEntitiesToDomain(it)
+        }
     }
 
     override suspend fun insertItem(item: Item) {
-        TODO("Not yet implemented")
+        val domainItem = DataMapper.mapDomainToEntity(item)
+        return localDataSource.insertItem(domainItem)
     }
 
     override suspend fun deleteItem(item: Item): Int {
-        TODO("Not yet implemented")
+        val domainItem = DataMapper.mapDomainToEntity(item)
+        return localDataSource.deleteItem(domainItem)
     }
 }

2fe99fe03ff47f5e27246823b42e75253f3ea7fe
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-05 07:54:41 +0700

    added null safety

--- Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/domain/model/Item.kt
+++ Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/domain/model/Item.kt
@@ -1,36 +1,35 @@
 package com.asthiseta.core.domain.model
 
 import android.os.Parcelable
-import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Item(
 
     //@field:Json(name = "_id")
-    val _id : String,
+    val _id : String?,
 
     //@field:Json(name = "name")
-    val name : String,
+    val name : String?,
 
     //@field:Json(name = "description")
-    val description : String,
+    val description : String?,
 
     //@field:Json(name = "address")
-    val address : String,
+    val address : String?,
 
     //@field:Json(name = "imageUrl")
-    val imageUrl: String,
+    val imageUrl: String?,
 
     //@field:Json(name = "genderRestriction")
-    val genderRestriction : String,
+    val genderRestriction : String?,
 
     //@field:Json(name = "available_bedrooms")
-    val available_bedrooms : Int,
+    val available_bedrooms : Int?,
 
     //@field:Json(name = "total_bedrooms")
-    val total_bedrooms : Int,
+    val total_bedrooms : Int?,
 
     //@field:Json(name = "price")
-    val price : Int
+    val price : Int?
 ):Parcelable

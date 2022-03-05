de446e1d307f89a2d7260691a7621091981f2ed7
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-05 07:28:10 +0700

    edited item

--- Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/domain/model/Item.kt
+++ Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/domain/model/Item.kt
@@ -7,30 +7,30 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Item(
 
-    @field:Json(name = "_id")
+    //@field:Json(name = "_id")
     val _id : String,
 
-    @field:Json(name = "name")
+    //@field:Json(name = "name")
     val name : String,
 
-    @field:Json(name = "description")
+    //@field:Json(name = "description")
     val description : String,
 
-    @field:Json(name = "address")
+    //@field:Json(name = "address")
     val address : String,
 
-    @field:Json(name = "imageUrl")
+    //@field:Json(name = "imageUrl")
     val imageUrl: String,
 
-    @field:Json(name = "genderRestriction")
+    //@field:Json(name = "genderRestriction")
     val genderRestriction : String,
 
-    @field:Json(name = "available_bedrooms")
+    //@field:Json(name = "available_bedrooms")
     val available_bedrooms : Int,
 
-    @field:Json(name = "total_bedrooms")
+    //@field:Json(name = "total_bedrooms")
     val total_bedrooms : Int,
 
-    @field:Json(name = "price")
+    //@field:Json(name = "price")
     val price : Int
 ):Parcelable

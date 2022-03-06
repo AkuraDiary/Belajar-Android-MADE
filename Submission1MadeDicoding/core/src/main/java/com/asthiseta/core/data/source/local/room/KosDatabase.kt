81aeebaa1d321df10edac9b7c31847bbecbbedb7
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-06 08:19:50 +0700

    added KosDatabase

--- Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/data/source/local/room/KosDatabase.kt
+++ Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/data/source/local/room/KosDatabase.kt
@@ -1,4 +1,10 @@
 package com.asthiseta.core.data.source.local.room
 
-abstract class KosDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.asthiseta.core.data.source.local.entity.KosEntity
+
+@Database(entities = [KosEntity::class],version=1, exportSchema = false)
+abstract class KosDatabase : RoomDatabase(){
+    abstract fun itemDao() : KosDao
 }

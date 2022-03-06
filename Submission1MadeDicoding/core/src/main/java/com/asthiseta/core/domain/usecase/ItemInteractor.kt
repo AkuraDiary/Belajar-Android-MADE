bdeb1740b7d1eabc1138893242af61bf6297e9c9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-06 09:57:22 +0700

    added code in item interactor

--- Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/domain/usecase/ItemInteractor.kt
+++ Submission1MadeDicoding/core/src/main/java/com/asthiseta/core/domain/usecase/ItemInteractor.kt
@@ -1,4 +1,27 @@
 package com.asthiseta.core.domain.usecase
 
-class ItemInteractor {
+import com.asthiseta.core.data.ItemRepos
+import com.asthiseta.core.data.Resource
+import com.asthiseta.core.domain.model.Item
+import kotlinx.coroutines.flow.Flow
+
+class ItemInteractor(private val itemRepos: ItemRepos) : ItemUseCase {
+    override fun getAllKos(query: String?): Flow<Resource<List<Item>>> {
+        return itemRepos.getAllKos(query)
+    }
+
+    override fun getDetailKos(name: String): Flow<Resource<Item>> {
+        return itemRepos.getDetailKos(name)
+    }
+
+    override fun getFavoriteItem(): Flow<List<Item>> {
+        return itemRepos.getFavoriteKos()
+    }
+
+    override suspend fun insertItem(item: Item) =
+        itemRepos.insertItem(item)
+
+
+    override suspend fun deleteItem(item: Item): Int =
+        itemRepos.deleteItem(item)
 }

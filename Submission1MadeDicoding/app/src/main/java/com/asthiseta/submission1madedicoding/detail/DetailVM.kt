7e3a5fac111c20f8a2d641b598ed43ee07664881
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-06 10:11:42 +0700

    added ViewModels

---
+++ Submission1MadeDicoding/app/src/main/java/com/asthiseta/submission1madedicoding/detail/DetailVM.kt
@@ -0,0 +1,21 @@
+package com.asthiseta.submission1madedicoding.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.asthiseta.core.domain.model.Item
+import com.asthiseta.core.domain.usecase.ItemUseCase
+import kotlinx.coroutines.launch
+
+class DetailVM(private val itemUseCase: ItemUseCase) : ViewModel(){
+
+    fun detailItem(name : String) = itemUseCase.getDetailKos(name).asLiveData()
+
+    fun insertFav(item:Item) = viewModelScope.launch {
+        itemUseCase.insertItem(item)
+    }
+
+    fun deleteFav(item : Item) = viewModelScope.launch {
+        itemUseCase.deleteItem(item)
+    }
+}

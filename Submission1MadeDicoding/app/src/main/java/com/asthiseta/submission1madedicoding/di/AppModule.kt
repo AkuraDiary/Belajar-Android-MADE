13fb41e485f78c9ab8c64a4a5facdae66c7aab3f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-07 09:21:05 +0700

    added app mdoule

--- Submission1MadeDicoding/app/src/main/java/com/asthiseta/submission1madedicoding/di/AppModule.kt
+++ Submission1MadeDicoding/app/src/main/java/com/asthiseta/submission1madedicoding/di/AppModule.kt
@@ -1,2 +1,17 @@
 package com.asthiseta.submission1madedicoding.di
 
+import com.asthiseta.core.domain.usecase.ItemInteractor
+import com.asthiseta.core.domain.usecase.ItemUseCase
+import com.asthiseta.submission1madedicoding.detail.DetailVM
+import com.asthiseta.submission1madedicoding.home.HomeVM
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<ItemUseCase> {ItemInteractor(get())}
+}
+
+val viewModelModule = module {
+    viewModel { HomeVM(get()) }
+    viewModel { DetailVM(get()) }
+}

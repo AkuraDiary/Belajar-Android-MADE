fb0271f72ed5bf161a65f857f0e9a6d6dc03daea
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-08 10:37:06 +0700

    added My Application Codes

--- Submission1MadeDicoding/app/src/main/java/com/asthiseta/submission1madedicoding/MyApplication.kt
+++ Submission1MadeDicoding/app/src/main/java/com/asthiseta/submission1madedicoding/MyApplication.kt
@@ -1,6 +1,31 @@
 package com.asthiseta.submission1madedicoding
 
 import android.app.Application
+import com.asthiseta.core.di.databaseModule
+import com.asthiseta.core.di.networkModule
+import com.asthiseta.core.di.repositoryModule
+import com.asthiseta.submission1madedicoding.di.useCaseModule
+import com.asthiseta.submission1madedicoding.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin{
+            androidLogger(Level.NONE)
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    databaseModule,
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
+                )
+            )
+        }
+    }
 }

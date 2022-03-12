624415f54e71c5418216560b45c61ae19534646e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-13 04:10:48 +0700

    added layout to containt the maps

--- MyMaps/app/src/main/java/com/asthiseta/myapplication/MainActivity.kt
+++ MyMaps/app/src/main/java/com/asthiseta/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.asthiseta.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(applicationContext, getString(R.string.mapbox_acces_token))
         setContentView(R.layout.activity_main)
     }
 }

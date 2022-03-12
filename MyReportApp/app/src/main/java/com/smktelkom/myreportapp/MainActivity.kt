6051feb14f1545e553e955c49ff6edd5b1b48ace
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-13 03:12:39 +0700

    aded implementation of Timber

--- MyReportApp/app/src/main/java/com/smktelkom/myreportapp/MainActivity.kt
+++ MyReportApp/app/src/main/java/com/smktelkom/myreportapp/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.ktx.Firebase
+import timber.log.Timber
+import java.lang.Exception
 import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +17,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Timber.d("Test debugging")
+
         val btnCrash = findViewById<Button>(R.id.btn_crash)
 
         // Obtain the FirebaseAnalytics instance.
         analytics = Firebase.analytics
 
         btnCrash.setOnClickListener{
-            throw RuntimeException("Test Crash")
+            FirebaseCrashlytics.getInstance().log("Clicked on button")
+            FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some data")
+            try{
+                throw RuntimeException("Test Crash")
+            }catch (e:Exception){
+                Timber.e("Test non Fatal Exception")
+            }
         }
     }
 }
---
+++ MyReportApp/app/src/main/java/com/smktelkom/myreportapp/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.smktelkom.myreportapp
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}

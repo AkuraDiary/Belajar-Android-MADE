8c71471ecd1e9916b724b6a57cdd1ae1d53a2e9e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-12 20:02:48 +0700

    added encryption

--- TourismApp/core/src/main/java/com/dicoding/tourismapp/core/di/CoreModule.kt
+++ TourismApp/core/src/main/java/com/dicoding/tourismapp/core/di/CoreModule.kt
@@ -1,5 +1,6 @@
 package com.dicoding.tourismapp.core.di
 
+import net.sqlcipher.database.SQLiteDatabase
 import androidx.room.Room
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.local.LocalDataSource
@@ -8,6 +9,7 @@ import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import com.dicoding.tourismapp.core.utils.AppExecutors
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,10 +21,14 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<TourismDatabase>().tourismDao() }
     single {
+        val passphrase : ByteArray =  SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             TourismDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 

7fc918bda161d2629fb9afcf44a4fa7f2f12af7c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-13 02:36:57 +0700

    added intentional crasher awkawkaw

--- MyReportApp/app/src/main/java/com/smktelkom/myreportapp/MainActivity.kt
+++ MyReportApp/app/src/main/java/com/smktelkom/myreportapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.smktelkom.myreportapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCrash = findViewById<Button>(R.id.btn_crash)
+
+        btnCrash.setOnClickListener{
+            throw RuntimeException("Test Crash")
+        }
     }
 }

4b69ebbf643ff644a8ec376cd6f56f28de626bba
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-13 02:58:37 +0700

    trying reconfiguring firebase

--- MyReportApp/app/src/main/java/com/smktelkom/myreportapp/MainActivity.kt
+++ MyReportApp/app/src/main/java/com/smktelkom/myreportapp/MainActivity.kt
@@ -3,15 +3,20 @@ package com.smktelkom.myreportapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var analytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnCrash = findViewById<Button>(R.id.btn_crash)
-
+        // Obtain the FirebaseAnalytics instance.
+        analytics = Firebase.analytics
         btnCrash.setOnClickListener{
             throw RuntimeException("Test Crash")
         }

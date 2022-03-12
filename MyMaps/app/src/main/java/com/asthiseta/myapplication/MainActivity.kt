952afa08ac27ca2c33d26bc0d859f5055003dc40
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-13 04:17:10 +0700

    implementing basic mapbox

--- MyMaps/app/src/main/java/com/asthiseta/myapplication/MainActivity.kt
+++ MyMaps/app/src/main/java/com/asthiseta/myapplication/MainActivity.kt
@@ -2,12 +2,67 @@ package com.asthiseta.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapView : MapView
+    private lateinit var btnNavigation : Button
+    private lateinit var mapboxMap : MapboxMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Mapbox.getInstance(applicationContext, getString(R.string.mapbox_acces_token))
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        btnNavigation = findViewById(R.id.btnNavigation)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {mapboxMap ->
+            this.mapboxMap = mapboxMap
+            mapboxMap.setStyle(Style.MAPBOX_STREETS){ style ->
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }

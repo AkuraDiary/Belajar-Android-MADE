bd3889256d9efe3af33c3dd4892d5fd11c3334e5
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-12 19:43:09 +0700

    implementing the encryption

--- MySimpleLoginEncryption/app/src/main/java/com/dicoding/mysimplelogin/SessionManager.kt
+++ MySimpleLoginEncryption/app/src/main/java/com/dicoding/mysimplelogin/SessionManager.kt
@@ -2,14 +2,41 @@ package com.dicoding.mysimplelogin
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.securepreferences.SecurePreferences
 
 class SessionManager(context: Context) {
-    companion object {
-        const val KEY_LOGIN = "isLogin"
-        const val KEY_USERNAME = "username"
+
+
+    private var pref: SharedPreferences = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+    //context.getSharedPreferences("Session", Context.MODE_PRIVATE)
+        val spec = KeyGenParameterSpec.Builder(
+            MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        )
+            .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+            .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+            .build()
+        val masterKey = MasterKey.Builder(context)
+            .setKeyGenParameterSpec(spec)
+            .build()
+        EncryptedSharedPreferences
+            .create(
+                context,
+                "Session",
+                masterKey,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+    } else {
+        SecurePreferences(context, "dicoding", "Session")
     }
 
-    private var pref: SharedPreferences = context.getSharedPreferences("Session", Context.MODE_PRIVATE)
     private var editor: SharedPreferences.Editor = pref.edit()
 
     fun createLoginSession() {
@@ -27,5 +54,10 @@ class SessionManager(context: Context) {
     fun saveToPreference(key: String, value: String) = editor.putString(key, value).commit()
 
     fun getFromPreference(key: String) = pref.getString(key, "")
+
+    companion object {
+        const val KEY_LOGIN = "isLogin"
+        const val KEY_USERNAME = "username"
+    }
 
 }

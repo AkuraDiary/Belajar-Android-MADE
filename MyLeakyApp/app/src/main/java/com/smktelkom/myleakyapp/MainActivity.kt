89de243ece452328288cc66d99bb4b619b13baa2
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-12 13:05:48 +0700

    latihan my leaky app

--- MyLeakyApp/app/src/main/java/com/smktelkom/myleakyapp/MainActivity.kt
+++ MyLeakyApp/app/src/main/java/com/smktelkom/myleakyapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.smktelkom.myleakyapp
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var broadcastReceiver: BroadcastReceiver
+    lateinit var tv_power_status  : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_power_status = findViewById(R.id.tv_power_status)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadCastReciever()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
+    }
+
+    private fun registerBroadCastReciever(){
+        broadcastReceiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when(intent?.action){
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        tv_power_status.text = getString(R.string.power_connected)
+                    }
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        tv_power_status.text = getString(R.string.power_disconnected)
+                    }
+                }
+            }
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(broadcastReceiver, intentFilter)
     }
 }

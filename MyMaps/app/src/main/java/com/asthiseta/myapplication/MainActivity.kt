4891cd7b0d2eeb803a4965720fc75ace5c1f5dec
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-13 08:03:16 +0700

    added marker

--- MyMaps/app/src/main/java/com/asthiseta/myapplication/MainActivity.kt
+++ MyMaps/app/src/main/java/com/asthiseta/myapplication/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.asthiseta.myapplication
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mapView : MapView
     private lateinit var btnNavigation : Button
     private lateinit var mapboxMap : MapboxMap
+    private lateinit var symbolManager: SymbolManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +33,34 @@ class MainActivity : AppCompatActivity() {
         mapView.getMapAsync {mapboxMap ->
             this.mapboxMap = mapboxMap
             mapboxMap.setStyle(Style.MAPBOX_STREETS){ style ->
+                symbolManager = SymbolManager(mapView, mapboxMap, style)
+                symbolManager.iconAllowOverlap = true
+
+                style.addImage(
+                    ICON_ID,
+                    BitmapFactory.decodeResource(resources, com.mapbox.mapboxsdk.R.drawable.mapbox_marker_icon_default)
+                )
+
+                showDicodingSpace()
             }
         }
+    }
+
+    private fun showDicodingSpace() {
+        val dicodingSpace = LatLng(-6.8957643, 107.6338462)
+        symbolManager.create(
+            SymbolOptions()
+                .withLatLng(LatLng(dicodingSpace.latitude, dicodingSpace.longitude))
+                .withIconImage(ICON_ID)
+                .withIconSize(1.5f)
+                .withIconOffset(arrayOf(0f, -1.5f))
+                .withTextField("Dicoding Space")
+                .withTextHaloColor("rgba(255, 255, 255, 100)")
+                .withTextHaloWidth(0.5f)
+                .withTextAnchor("top")
+                .withDraggable(true)
+        )
+        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dicodingSpace, 8.0))
     }
 
     override fun onStart() {
@@ -64,5 +96,9 @@ class MainActivity : AppCompatActivity() {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val ICON_ID = "ICON_ID"
     }
 }
